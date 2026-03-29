91ca77c41bd1bba2feb56704d20e723594467a19
Soniya Mirage <soniyamirage014.com>
2026-03-29 16:50:40 +0530

    connected backend

--- frontend_old/app.js
+++ frontend_old/app.js
@@ -1,1 +1,25 @@
-const API = "https://task-backend-jqxm.onrender.com"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const API = "https://task-manager-backend.onrender.com/api/"; // <-- तुझा backend URL
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(API)
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Task Manager 🚀</h1>
+      {data.map((item, index) => (
+        <p key={index}>{JSON.stringify(item)}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
